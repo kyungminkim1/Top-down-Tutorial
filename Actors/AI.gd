8295b882bc3b5cab4f593104c82294a6ff64a5fe
Kyung-Min <kyungminkimnz@gmail.com>
2022-09-04 20:22:50 +1200

    Add random movement for enemy AI A few problems to note: 1. The enemy will not start off in State.PATROL until the player walks in then out of their DetectionZone. 2. The enemy's random movement will replay every time the scene loads.

--- Actors/AI.gd
+++ Actors/AI.gd
@@ -11,18 +11,25 @@ onready var player_detection_zone = $PlayerDetectionZone
 onready var patrol_timer = $PatrolTimer
 
 var current_state: int = State.PATROL setget set_state
-var actor = null
+var actor: KinematicBody2D = null
 var player: Player = null
 var weapon: Weapon = null
 
 # PATROL STATE VARIABLES
 var origin: Vector2 = Vector2.ZERO
 var patrol_location = Vector2.ZERO
+var patrol_location_reached: bool = false
+var actor_velocity: Vector2 = Vector2.ZERO
 
 func _process(delta):
 	match current_state:
 		State.PATROL:
-			pass
+			if not patrol_location_reached:
+				actor.move_and_slide(actor_velocity)
+				if actor.global_position.distance_to(patrol_location) < 5:
+					patrol_location_reached = true
+					actor_velocity = Vector2.ZERO
+					patrol_timer.start()
 		State.ENGAGE:
 			if player != null and weapon != null:
 				var angle_to_player = actor.global_position.direction_to(player.global_position).angle()
@@ -46,7 +53,9 @@ func set_state(new_state: int):
 	if new_state == current_state:
 		return
 	if new_state == State.PATROL:
+		origin = actor.global_position
 		patrol_timer.start()
+		patrol_location_reached = true
 	
 	current_state = new_state
 	emit_signal("state_changed", current_state)
@@ -70,3 +79,5 @@ func _on_PatrolTimer_timeout():
 	var random_x = rand_range(-patrol_range, patrol_range)
 	var random_y = rand_range(-patrol_range, patrol_range)
 	patrol_location = Vector2(random_x, random_y) + origin
+	patrol_location_reached = false
+	actor_velocity = actor.global_position.direction_to(patrol_location) * 100
