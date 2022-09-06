9075ccba3b45eae65a1b4cedd2cbedbacba19076
Kyung-Min <kyungminkimnz@gmail.com>
2022-09-06 18:54:28 +1200

    Fix condition required to emit signal

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -28,7 +28,8 @@ func shoot():
 		shoot_cooldown.start()
 		animation_player.play("muzzle_flash")
 		current_ammo -= 1
-		emit_signal("weapon_out_of_ammo")
+		if current_ammo == 0
+			emit_signal("weapon_out_of_ammo")
 
 func start_reload():
 	animation_player.start("reload")
