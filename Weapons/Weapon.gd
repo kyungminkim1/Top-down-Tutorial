2b73e1337f118b341ef9f2aca613c9cb9d104176
Kyung-Min <kyungminkimnz@gmail.com>
2022-09-06 18:55:13 +1200

    Add forgotton colon

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -28,7 +28,7 @@ func shoot():
 		shoot_cooldown.start()
 		animation_player.play("muzzle_flash")
 		current_ammo -= 1
-		if current_ammo == 0
+		if current_ammo == 0:
 			emit_signal("weapon_out_of_ammo")
 
 func start_reload():
